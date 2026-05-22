80be2e85ea8b0ccead6db065c98bac54997df4ff
Sebastián  Agustín Miño <sebastianagustinmino@gmail.com>
2026-05-22 13:48:19 -0300

    arrays, foreach y composicion de funciones

---
+++ arrayeobj.js
@@ -0,0 +1,84 @@
+const evaluateAge =(name, age, stundent) => {
+//Si aplicamos un condicional, primero se aplica la condicion y despues la logica    
+if(typeof name !== "string"){
+    console.error("Error: Name has to be a text")
+    return
+}
+if(typeof age !== "number"){
+    console.error('Error:Age has to be a number' )
+return
+}
+if(typeof stundent !== "boolean"){
+    console.error("Error: Student tiene que ser un boolean")
+    return
+}
+//logica para que ingrese los datos correctos
+
+//here's going to return a boolean
+
+let mensaje = ``
+if (age < 18){
+    mensaje = `menor`
+} else if (age >= 18 && age <= 90){
+    mensaje = `aceptado`
+} else {
+    mensaje= `mayor`
+}
+return mensaje
+}
+
+const enrollStudent = (obj,callback)=>{
+    //Here I call the callback, within the function it receives arguments from the objects
+    //then that data is saved in a variable to see if it is true or false.
+    // Then you appear the data with a condition, if it is met you return a message with the return below
+
+    const resultCallback = callback(obj.name, obj.age,obj.stundent)
+    
+    let mensaje = ``
+    
+    if(resultCallback === `menor`){
+    mensaje = `RECHAZADO. No podes aprender. Lo siento, sos menor ${obj.name}`
+	} else if(resultCallback === `aceptado`){
+    mensaje =`ACEPTADO ${obj.name} podes inscribirte!`
+	} else {
+    mensaje = `RECHAZADO sos Mayor de edad. ${obj.name}` 
+}
+return mensaje
+
+}
+
+
+let students = [
+    { name: "Sebastian", age: 28,  stundent: true, lenguaje: "Javascript" },
+    { name: "Jorge", age: 34,  stundent: false, lenguaje: "Python" },
+    {name: "April", age:21,  stundent: true, lenguaje: "css"},
+    {name: "Pablo", age:17,  stundent: false, lenguaje: "C++"}
+]
+
+
+//foreach to traverse the array
+//student is a temporary variable of the array path
+//becomes an object and you can access the data within the
+
+
+students.forEach( student => {
+//we run the enrollstudent function
+
+const answer = enrollStudent(student, evaluateAge)
+
+//MODIFIED ENROLLSTUDENT FUNCTION MESSAGE
+
+if(answer.includes("ACEPTADO")){
+    console.log(`CONGRATULATIONS
+        Answer: you ARE ACCEPTED, THANKS FOR TRUSTING ${student.name}
+        Exitos con tu nuevo lenguaje de programacion ${student.lenguaje}
+        `)
+	} else {
+    console.log(`Sorry ${student.name}, Rejected!
+        Cuando haya una segunda oportunidad. Pensaremos en ti! ${student.name}
+        `)
+}
+
+});
+
+
