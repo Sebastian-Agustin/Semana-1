f3f2acbd184361a996a630a490b3c3387db40904
Sebastián  Agustín Miño <sebastianagustinmino@gmail.com>
2026-05-28 17:01:12 -0300

    logica de buscador terminada, captura el valor del input, devuelve array, convierto los datos del array en string y los muestro en pantalla con un map. y una estructura li dentro del array, hoy aprendi capturar elementos, escribir contenido dentro del un contenido de html. y la diferencia entre appchild y innerhtml

--- mini-Proyecto/buscador/index.js
+++ mini-Proyecto/buscador/index.js
@@ -11,11 +11,21 @@ inputSearch.addEventListener("input", ()=>{
     //I anticipated because if the user writes in lowercase the language they are looking for
     //I wasn't going to find it, that's why I used the tolowercase function. So that the elements of the array that are inside
     //sean minuscula, y lo que escribe el usuario tambien. Y pueda retornar el valor que esta buscando 
-    const answer = lenguajes.filter( element=> element.toLowerCase().includes(inputSearch.value.toLowerCase()))
-    console.log(typeof answer)
-    
-    if(typeof answer){
-        
-    }
+    const answer = lenguajes.filter( element => element.toLowerCase().includes(inputSearch.value.toLowerCase()))
+    console.log(answer.join(", "))// this is a way for you to return the array in a string. USING the JOIN METHOD
+    //If there is  another way that
 
+
+    //map esta recorriendo el array 
+    ulContainer.innerHTML = answer.map( element => `<li>${element}</li>`).join("")//aca convierto la respuesta del answer en string
+    //ESTE ES EL MEJOR PARA MI
+    // const arrayObjetLanguages = answer.map({ 
+    // })
 })
+
+
+
+//Something I learned today, today if you want to create functional components for html
+//and you can also create content inside the html
+//that's why you can capture html container element. call it with the .innerthtml attribute and pass it with a map the entire structure of that element if it is an array its response
+//sino podes armar los componentes y darle lo que quieras retornar
